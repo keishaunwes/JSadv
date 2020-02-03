d45887416b39e281f27d2dfa6d350ec157dc37c3
keishaun wesley <keishaunwesley@gmail.com>
2020-02-03 15:13:40 -0600

    finally

---
+++ hoisting.js
@@ -0,0 +1,47 @@
+setName()
+
+function setName(){
+    var name = "keishaun";
+    console.log(name)
+}
+
+
+console.log("some text")
+let avg = avgFr(2,2)
+console.log("some text", avg)
+
+function avgFr(a,b){
+    console.log("some text")
+    result = (a-b)/2
+    return result
+    
+}
+
+fruits = ["apple","orange","grapes"]
+
+function showFruits(){
+let favFruit = fruits[0];
+console.log(fruits[2])
+
+function displayingFv(){
+    console.log(favFruit)
+}  
+displayingFv()
+}
+
+theGreeting()
+function theGreeting(){
+    console.log("hello "+ name)
+    
+}
+let alerts = function (){
+    alert("wassup everybody its me the coder")
+}
+
+alerts()
+let leastFav = "strawberries"
+
+
+
+showFruits()
+console.log(leastFav)
